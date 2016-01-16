2d8a1df8652dfc192120463006577e02922433ca
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-01-16 13:12:30 -0500

    E1_Q8

--- people.rb
+++ people.rb
@@ -20,6 +20,10 @@ class Instructor < Name
   end
 end
 
+class Test
+end
+
+
 #Question 6
 chris = Instructor.new("Chris")
 chris.greeting
@@ -27,3 +31,15 @@ chris.greeting
 #Question 7
 christina = Student.new("Christina")
 christina.greeting
+
+#Question 8
+#This works because:
+#Class Instructor inherits from Class Name
+#Therefore calling the new method on Class Instructor
+#Effectively initializes the instance variable @name
+#And instantiates a new class object
+#giving access to all features of both
+#the Name and Instructor classes for chris
+#and the Name and Student classes for christina
+chris.teach
+christina.learn
